5192da02b68005fa0a21a8f13b6c945678453706
mroley1 <64291507+mroley1@users.noreply.github.com>
2023-12-21 18:18:29 -0500

    token movement correction

--- ui/src/components/moveables/MoveBoard.tsx
+++ ui/src/components/moveables/MoveBoard.tsx
@@ -15,16 +15,19 @@ function MoveBoard() {
       function (index: number) {
         return function (event: any) {
           
+          // check that game mode is one that allows items to be moved
           if (!MOVEMENT_ALLOWED.includes(gameContext.state.gameMode)) {
             return
           }
           
+          // set high z- index and grabbing cursor
           event.target.style.zIndex = "30"
           event.target.style.cursor = "grabbing"
           
           var xOffset = 0
           var yOffset = 0
         
+          // determine event type and calculate offesets
           if (event.type === "mousedown") {
             xOffset = event.clientX - event.target.getBoundingClientRect().x
             yOffset = event.clientY - event.target.getBoundingClientRect().y
@@ -37,29 +40,51 @@ function MoveBoard() {
           
           const drag = (function(event: any) {
             var target = document.getElementById(elemId)
+            // error catching
             if (target == null) {
               console.error("tried to drag nonexistant item")
               dragUp()
             }
+            // set position of token
             target!.style.top = (event.clientY - yOffset).toString() + "px"
             target!.style.left = (event.clientX - xOffset).toString() + "px"
           })
           
           const dragUp = (function() {
+            // remove listeners
             document.onpointermove = null
             document.onpointerup = null
+            // error catching
             const target = document.getElementById(elemId)
             if (target === undefined || target === null) {
               console.error("could not find token to update position")
+              return
             }
+            // reset z index and cursor type
             target!.style.zIndex = "";
             target!.style.cursor = "grab";
+            // correct tokens to keep them on the page
+            let boundingRect = target.getBoundingClientRect()
+            let width = window.innerWidth
+            let height = window.innerHeight
+            if (boundingRect.left < 0) {
+              target.style.left = "0px"
+            } else if (boundingRect.right > width) {
+              target.style.left = (width - boundingRect.width) + "px"
+            }
+            if (boundingRect.top < 0) {
+              target.style.top = "0px"
+            } else if (boundingRect.bottom > height) {
+              target.style.top = (height - boundingRect.height) + "px"
+            }
+            // save to state
             const tmp = JSON.parse(JSON.stringify(gameContext.state));
             tmp["tokens"][index].xpos = target?.getBoundingClientRect().x
             tmp["tokens"][index].ypos = target?.getBoundingClientRect().y
             gameContext.setter(tmp)
           })
           
+          // set listeners
           document.onpointermove = drag
           document.onpointerup = dragUp
         }
