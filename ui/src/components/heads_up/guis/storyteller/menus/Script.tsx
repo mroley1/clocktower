900b62152236e9ea472678e071b7b30babd4ecb6
mroley1 <64291507+mroley1@users.noreply.github.com>
2023-12-31 14:47:47 -0500

    this api stuff is hard

--- ui/src/components/heads_up/guis/storyteller/menus/Script.tsx
+++ ui/src/components/heads_up/guis/storyteller/menus/Script.tsx
@@ -3,10 +3,40 @@ import './Menus.scss';
 import { GameContext } from '@/components/App';
 import { HeadsUpContext } from '@/components/heads_up/HeadsUp';
 
+declare const window: any;
+
 function Script(props: any) {
     
     const gameContext = useContext(GameContext)
     const headsUpContext = useContext(HeadsUpContext)
+    
+    const pickerOpts = {
+        types: [
+          {
+            description: "application/json",
+            accept: {
+              "data/*": [".json"],
+            },
+          },
+        ],
+        excludeAcceptAllOption: true,
+        multiple: false,
+      };
+      
+    async function getTheFile() {
+    try {
+        const [fileHandle]: FileSystemFileHandle[] = await window.showOpenFilePicker(pickerOpts);
+        const fileData = await fileHandle.getFile();
+        //const accessor = await fileHandle.createWritable();
+        //let data = accessor.data
+        console.log(window.isSecureContext)
+        console.log(fileHandle)
+        //accessor.close()
+    } catch (error: any) {
+        if (error.name === "AbortError") {return}
+        console.error(error)
+    }
+    }
     
     useEffect(() => {
         return () => {
@@ -18,6 +48,7 @@ function Script(props: any) {
         <div id='radialMenuActionSolidBase'>
             <div className='close' onClick={()=>{headsUpContext.util.toggleRadialMenuState(false)}}></div>
             <div className='center'></div>
+            <input type='button' value='test' onClick={getTheFile}></input>
                 
         </div>
     );
