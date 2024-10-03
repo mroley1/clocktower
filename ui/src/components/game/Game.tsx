33cd888664264ae5fc9b24468690218a54d7a1ef
Matthew Oley <64291507+mroley1@users.noreply.github.com>
2024-10-03 19:26:59 -0400

    create game component

---
+++ ui/src/components/game/Game.tsx
@@ -0,0 +1,13 @@
+import { createContext } from 'react';
+import './App.scss';
+
+function Game() {
+  
+  const GameContext = createContext({})
+  
+  return (
+    <></>
+  );
+}
+
+export default Game;
