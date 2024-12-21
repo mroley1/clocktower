9ae54906b5ea919c244b4c3733bc92393d7136aa
mroley1 <oleymr22@gmail.com>
2024-12-21 14:42:06 -0500

    remove NightGuide

--- ui/src/components/game/nightGuide/NightGuide.tsx
+++ ui/src/components/game/nightGuide/NightGuide.tsx
@@ -1,109 +1,109 @@
-import { useContext, useEffect, useState } from "react";
-import style from "./NightGuide.module.scss";
-import { ControllerContext, DataContext, GameContext } from "../Game";
-import { Alignmant } from "../../../components/common/RoleType";
-import { ReferenceData } from "@/components/common/ReferenceData";
-import { CSSTransition } from "react-transition-group"
+// import { useContext, useEffect, useState } from "react";
+// import style from "./NightGuide.module.scss";
+// import { ControllerContext, ReferenceContext, GameContext } from "../Game";
+// import { Alignmant } from "../../../components/common/RoleType";
+// import { ReferenceData } from "@/components/common/ReferenceData";
+// import { CSSTransition } from "react-transition-group"
 
-function NightGuide() {
+// function NightGuide() {
     
-    const dataContext = useContext(DataContext)
-    const gameContext = useContext(GameContext)
-    const controllerContext = useContext(ControllerContext)
+//     const referenceContext = useContext(ReferenceContext)
+//     const gameContext = useContext(GameContext)
+//     const controllerContext = useContext(ControllerContext)
     
-    var nightOrder: ReferenceData.NightOrderTurn[] = []
-    if (gameContext.gameProgression.night) {
-        if (gameContext.gameProgression.night == 1) {
-            nightOrder = dataContext.nightOrder.getFirstNight(gameContext.players);
-        } else {
-            nightOrder = dataContext.nightOrder.getOtherNight(gameContext.players);
-        }
-    }
+//     var nightOrder: ReferenceData.NightOrderTurn[] = []
+//     if (gameContext.gameProgression.night) {
+//         if (gameContext.gameProgression.night == 1) {
+//             nightOrder = referenceContext.nightOrder.getFirstNight(gameContext.players);
+//         } else {
+//             nightOrder = referenceContext.nightOrder.getOtherNight(gameContext.players);
+//         }
+//     }
     
-    let initalFocusedIndex = 0
-    if (nightOrder.length > 0 && gameContext.gameProgression.isNight) {
-        initalFocusedIndex = nightOrder.findIndex((value) => value.UUID == gameContext.gameProgression.currentTurn)
-        if (initalFocusedIndex < 0) {
-            initalFocusedIndex = 0
-        }
+//     let initalFocusedIndex = 0
+//     if (nightOrder.length > 0 && gameContext.gameProgression.isNight) {
+//         initalFocusedIndex = nightOrder.findIndex((value) => value.UUID == gameContext.gameProgression.currentTurn)
+//         if (initalFocusedIndex < 0) {
+//             initalFocusedIndex = 0
+//         }
         
-    }
+//     }
     
-    const [focusedIndex, setFocusedIndex] = useState(initalFocusedIndex)
+//     const [focusedIndex, setFocusedIndex] = useState(initalFocusedIndex)
     
-    useEffect(() => {
-        if (gameContext.gameProgression.currentTurn && nightOrder.length > 0) {
-            if (gameContext.gameProgression.isNight) {
-                let focusedIndex = nightOrder.findIndex((value) => value.UUID == gameContext.gameProgression.currentTurn)
-                if (focusedIndex < 0) {
-                    focusedIndex = 0
-                }
-                setFocusedIndex(focusedIndex)
-            }
-        }
-    }, [gameContext.gameProgression.currentTurn])
+//     useEffect(() => {
+//         if (gameContext.gameProgression.currentTurn && nightOrder.length > 0) {
+//             if (gameContext.gameProgression.isNight) {
+//                 let focusedIndex = nightOrder.findIndex((value) => value.UUID == gameContext.gameProgression.currentTurn)
+//                 if (focusedIndex < 0) {
+//                     focusedIndex = 0
+//                 }
+//                 setFocusedIndex(focusedIndex)
+//             }
+//         }
+//     }, [gameContext.gameProgression.currentTurn])
     
-    function setCurrentTurn(focusedIndex: number) {
-        gameContext.gameProgression.currentTurn = nightOrder[focusedIndex].UUID
-        setFocusedIndex(focusedIndex)
-    }
-    function next(event: React.PointerEvent<HTMLDivElement>) {
-        event.stopPropagation();
-        if (focusedIndex < nightOrder.length) {
-            setCurrentTurn(focusedIndex + 1)
-        }
-    }
-    function last(event: React.PointerEvent<HTMLDivElement>) {
-        event.stopPropagation();
-        if (focusedIndex >= 0) {
-            setCurrentTurn(focusedIndex - 1)
-        }
-    }
+//     function setCurrentTurn(focusedIndex: number) {
+//         gameContext.gameProgression.currentTurn = nightOrder[focusedIndex].UUID
+//         setFocusedIndex(focusedIndex)
+//     }
+//     function next(event: React.PointerEvent<HTMLDivElement>) {
+//         event.stopPropagation();
+//         if (focusedIndex < nightOrder.length) {
+//             setCurrentTurn(focusedIndex + 1)
+//         }
+//     }
+//     function last(event: React.PointerEvent<HTMLDivElement>) {
+//         event.stopPropagation();
+//         if (focusedIndex >= 0) {
+//             setCurrentTurn(focusedIndex - 1)
+//         }
+//     }
     
-    const [showDescription, setShowDescription] = useState(false)
-    function toggleDescription(event: React.PointerEvent<HTMLDivElement>) {
-        event.stopPropagation()
-        setShowDescription(!showDescription)
-    }
+//     const [showDescription, setShowDescription] = useState(false)
+//     function toggleDescription(event: React.PointerEvent<HTMLDivElement>) {
+//         event.stopPropagation()
+//         setShowDescription(!showDescription)
+//     }
     
-    return (
-        <div className={style.container} data-show={gameContext.gameProgression.isNight}>
-            <div className={style.turntable} style={{transform: `rotate(${focusedIndex*25}deg)`}} onClick={toggleDescription}>
-                {nightOrder.map((turnData, index) => {
-                    if (index > focusedIndex - 4 && index < focusedIndex + 4) {
-                        var callback = toggleDescription
-                        if (index < focusedIndex) {callback = last}
-                        if (index > focusedIndex) {callback = next}
-                        return (
-                            <CSSTransition key={turnData.UUID} in={showDescription} timeout={300} classNames={{...style}}>
-                                <div className={style.nightTurn} style={{transform: `rotate(${-index*25}deg)`}} >
-                                    <div className={style.tile} data-alignment={Alignmant[turnData.alignment]} onClick={callback}>
-                                        <img src={getImage(turnData.role, turnData.alignment)}></img>
-                                    </div>
-                                    <div className={style.description} style={{display: index==focusedIndex? undefined : "none"}}>
-                                        <div className={style.shapeLeft}></div>
-                                        <div className={style.shapeRight}></div>
-                                        <span>{turnData.description}</span>
-                                    </div>
-                                </div>
-                            </CSSTransition>
-                        )
-                    } else return null
-                })}
-            </div>
-        </div>
-    )
-}
+//     return (
+//         <div className={style.container} data-show={gameContext.gameProgression.isNight}>
+//             <div className={style.turntable} style={{transform: `rotate(${focusedIndex*25}deg)`}} onClick={toggleDescription}>
+//                 {nightOrder.map((turnData, index) => {
+//                     if (index > focusedIndex - 4 && index < focusedIndex + 4) {
+//                         var callback = toggleDescription
+//                         if (index < focusedIndex) {callback = last}
+//                         if (index > focusedIndex) {callback = next}
+//                         return (
+//                             <CSSTransition key={turnData.UUID} in={showDescription} timeout={300} classNames={{...style}}>
+//                                 <div className={style.nightTurn} style={{transform: `rotate(${-index*25}deg)`}} >
+//                                     <div className={style.tile} data-alignment={Alignmant[turnData.alignment]} onClick={callback}>
+//                                         <img src={getImage(turnData.role, turnData.alignment)}></img>
+//                                     </div>
+//                                     <div className={style.description} style={{display: index==focusedIndex? undefined : "none"}}>
+//                                         <div className={style.shapeLeft}></div>
+//                                         <div className={style.shapeRight}></div>
+//                                         <span>{turnData.description}</span>
+//                                     </div>
+//                                 </div>
+//                             </CSSTransition>
+//                         )
+//                     } else return null
+//                 })}
+//             </div>
+//         </div>
+//     )
+// }
 
-function getImage(role: string, alignment: Alignmant) {
-    if (role && alignment != Alignmant.NONE) {
-        if (alignment == Alignmant.GOOD) {
-            var imageName = role + "_good.png"
-        } else {
-            var imageName = role + "_evil.png"
-        }
-        return require("../../../assets/icons/" + imageName)
-    } else return null
-}
+// function getImage(role: string, alignment: Alignmant) {
+//     if (role && alignment != Alignmant.NONE) {
+//         if (alignment == Alignmant.GOOD) {
+//             var imageName = role + "_good.png"
+//         } else {
+//             var imageName = role + "_evil.png"
+//         }
+//         return require("../../../assets/icons/" + imageName)
+//     } else return null
+// }
 
-export default NightGuide
+// export default NightGuide
