4c7b48caf0d4aec032361fa0a6489838916fbcef
mroley1 <oleymr22@gmail.com>
2024-12-26 17:04:03 -0500

    style and integrate slider

--- ui/src/components/game/bag/Bag.tsx
+++ ui/src/components/game/bag/Bag.tsx
@@ -1,6 +1,7 @@
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useContext, useState } from 'react';
 import styles from './Bag.module.scss';
 import { ControllerContext, GameContext, ReferenceContext } from '../Game';
+import Slider from './Slider';
 
 interface BagProps {
     completeSetupFunc: () => void
@@ -11,7 +12,7 @@ function Bag({completeSetupFunc}: BagProps) {
     const gameContext = useContext(GameContext)
     const controllerContext = useContext(ControllerContext)
   
-    const [playerCountSlider, setPlayerCountSlider] = useState(7)
+    const [playerCountSlider, setPlayerCountSlider] = useState(gameContext.bag.quantity)
     function setPlayerCount(count: number) {
         setPlayerCountSlider(count)
     }
@@ -25,7 +26,6 @@ function Bag({completeSetupFunc}: BagProps) {
   
     return (
         <>
-            {playerCountSlider}
             <Slider playerCount={playerCountSlider} setPlayerCount={setPlayerCount}></Slider>
             {referenceContext.roles.roleList.map((role) => {
                 return (
@@ -40,56 +40,3 @@ function Bag({completeSetupFunc}: BagProps) {
 }
 
 export default Bag;
-
-interface SliderProps {
-    playerCount: number
-    setPlayerCount: (count: number) => void
-}
-function Slider({playerCount, setPlayerCount}: SliderProps) {
-    
-    let sliding = false
-    
-    function cancelSlide() {
-        document.removeEventListener("pointermove", move)
-        document.removeEventListener("pointerup", stopSlide)
-        document.removeEventListener("pointercancel", cancelSlide)
-    }
-    
-    function stopSlide() {
-        cancelSlide()
-        console.log(cursor.current)
-    }
-    
-    useEffect(() => {
-        return () => {
-            if (sliding) {
-                cancelSlide()
-            }
-    }}, [])
-    
-    const slider = useRef<HTMLDivElement>(null)
-    const cursor = useRef<HTMLDivElement>(null)
-    
-    function startSlide() {
-        sliding = true
-        document.addEventListener("pointermove", move)
-        document.addEventListener("pointerup", stopSlide)
-        document.addEventListener("pointercancel", cancelSlide)
-    }
-    
-    function move(e: PointerEvent) {
-        if (sliding && slider.current && cursor.current) {
-            e.preventDefault()
-            cursor.current.style.left = (e.clientX - slider.current.getBoundingClientRect().left - (cursor.current.getBoundingClientRect().width / 2)).toString() + "px"
-        }
-    }
-    
-    
-    return (
-        <div className={styles.slider_container}>
-            <div ref={slider} className={styles.slider}>
-                <div ref={cursor} className={styles.cursor} onPointerDown={startSlide}></div>
-            </div>
-        </div>
-    )
-}
---
+++ ui/src/components/game/bag/Slider.tsx
@@ -0,0 +1,117 @@
+import { useEffect, useRef } from 'react';
+import styles from './Bag.module.scss';
+
+interface SliderProps {
+    playerCount: number
+    setPlayerCount: (count: number) => void
+}
+function Slider({playerCount, setPlayerCount}: SliderProps) {
+    
+    const MINPLAYERCOUNT = 5;
+    const MAXPLAYERCOUNT = 15;
+    
+    let sliding = false
+    
+    const captureEvent = (e: any) => {
+        e.preventDefault()
+        e.stopPropagation()
+    }
+    
+    function getClosestBin(clientX: number) {
+        if (slider.current) {
+            const sliderRect = slider.current.getBoundingClientRect()
+            const leftReference = clientX - sliderRect.left
+            const binWidth = sliderRect.width / (MAXPLAYERCOUNT - MINPLAYERCOUNT + 1)
+            return Math.min(Math.max(Math.floor(leftReference / binWidth), 0), MAXPLAYERCOUNT - MINPLAYERCOUNT)
+        }
+        return 0
+    }
+    
+    function getClosestValueFromBinNumber(selectedBin: number) {
+        if (slider.current) {
+            console.log(selectedBin)
+            const sliderRect = slider.current.getBoundingClientRect()
+            const binWidth = sliderRect.width / (MAXPLAYERCOUNT - MINPLAYERCOUNT + 1)
+            return ((selectedBin + 0.5) * binWidth)
+        }
+        return 0
+    }
+    
+    function cancelSlide() {
+        document.removeEventListener("pointermove", move)
+        document.removeEventListener("pointerup", stopSlide)
+        document.removeEventListener("pointercancel", cancelSlide)
+    }
+    
+    useEffect(() => {
+        return () => {
+            if (sliding) {
+                cancelSlide()
+            }
+    }}, [])
+    
+    const slider = useRef<HTMLDivElement>(null)
+    const cursor = useRef<HTMLDivElement>(null)
+    
+    function cursorPositionToLeft(cursorPosition: number) {
+        if (cursor.current) {
+            return (cursorPosition - (cursor.current.getBoundingClientRect().width / 2)).toString() + "px"
+        }
+        return "0px"
+    }
+    
+    function startSlide(e: React.PointerEvent<HTMLDivElement>) {
+        updateHover(getClosestBin(e.clientX))
+        sliding = true
+        cursor.current?.setAttribute("data-moving", "true")
+        document.addEventListener("pointermove", move)
+        document.addEventListener("pointerup", stopSlide)
+        document.addEventListener("pointercancel", cancelSlide)
+    }
+    
+    function move(e: PointerEvent) {
+        if (sliding && slider.current && cursor.current) {
+            e.preventDefault()
+            updateHover(getClosestBin(e.clientX))
+            cursor.current.style.left = cursorPositionToLeft(e.clientX - slider.current.getBoundingClientRect().left)
+        }
+    }
+    
+    function stopSlide(e: PointerEvent) {
+        cancelSlide()
+        if (slider.current && cursor.current) {
+            cursor.current.setAttribute("data-moving", "false")
+            const closestBin = getClosestBin(e.clientX)
+            cursor.current.style.left = cursorPositionToLeft(getClosestValueFromBinNumber(closestBin));
+            setPlayerCount(closestBin + MINPLAYERCOUNT)
+        }
+    }
+    
+    const bins = Array.from({length: MAXPLAYERCOUNT - MINPLAYERCOUNT + 1}, (_, i) => MINPLAYERCOUNT + i)
+    
+    function updateHover(currentBin: number) {
+        if (slider.current) {
+            slider.current.querySelectorAll(`.${styles.bin}`).forEach((element) => {
+                element.setAttribute("data-hovering", (currentBin.toString() == element.getAttribute("data-index"))?"true":"false")
+            })
+        }
+    }
+    
+    return (
+        <div className={styles.slider_container} onClick={captureEvent} onDoubleClick={captureEvent}>
+            <div ref={slider} className={styles.slider} style={{gridTemplateColumns: `repeat(${bins.length}, 1fr)`}}>
+                <div ref={cursor} className={styles.cursor} onPointerDown={startSlide} data-moving="false" style={{left: cursorPositionToLeft(getClosestValueFromBinNumber(playerCount - MINPLAYERCOUNT))}}>
+                    <div className={styles.visible}></div>
+                </div>
+                {bins.map((binValue, index) => {
+                    return <div key={binValue} data-index={index} data-hovering={binValue == playerCount} className={styles.bin}>
+                        {binValue}
+                    </div>
+                })}
+            </div>
+        </div>
+    )
+}
+
+
+export default Slider
